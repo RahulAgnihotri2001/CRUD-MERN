248489428bc64b02b6e956589fc112d730b39d9e
Rahul Agnihotri <rahulagnihotri4444@gmail.com>
2024-09-18 10:23:58 +0530

    .env added

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
+
+const mongoURL = process.env.mongourl;
 mongoose
   .connect(
-    "mongodb+srv://username:1234@clusterrahul.tjfsgqu.mongodb.net/test?retryWrites=true&w=majority&appName=ClusterRahul"
+    // "mongodb+srv://username:1234@clusterrahul.tjfsgqu.mongodb.net/test?retryWrites=true&w=majority&appName=ClusterRahul"
+    mongoURL
   )
   .then(() => {
     console.log("Connected to database");
     app.listen(3000, () => {
-        console.log("Server Running on port 3000");
+      console.log("Server Running on port 3000");
     });
   })
   .catch((e) => {
@@ -18,5 +22,3 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
-
